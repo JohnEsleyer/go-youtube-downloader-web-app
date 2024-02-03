20dd3fc3bcb9544c3e9e2efce87defffd02a4046
JohnEsleyer <ralphesleyer@gmail.com>
2024-02-03 12:33:32 +0800

    added loading indicator during video processing

--- frontend/youtube-downloader/src/App.tsx
+++ frontend/youtube-downloader/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import LoadingSVG from './assets/loading.svg';
+import './App.css';
 import axios from 'axios';
 
 
@@ -9,44 +8,47 @@ function App() {
   const [url, setUrl] = useState<string>('');
   const [inputColor, setInputColor] = useState<string>('bg-gray-50 border text-black text-sm rounded-lg block w-80 p-2.5 bg-gray-100 border-gray-600 placeholder-gray-400 text-black mb-4');
   const [isErrorTextVisible, setErrorTextVisible] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(false); // New loading state
 
   const handleSubmit = async () => {
-    try{
-         // Send YouTube URL to the API
-         const response = await axios.post('http://localhost:8080/download', { url }, { responseType: 'blob' });
+    try {
+      setLoading(true);
 
-         console.log(response.data);
-   
-         // Create a Blob from the response data
-         const blob = new Blob([response.data], { type: 'video/mp4' });
-   
-         // Create a temporary URL for the Blob
-         const blobUrl = URL.createObjectURL(blob);
-   
-         // Create an anchor element to trigger the download
-         const anchor = document.createElement('a');
-         anchor.href = blobUrl;
-         anchor.download = 'video.mp4';
-         document.body.appendChild(anchor);
-   
-         // Trigger the download
-         anchor.click();
-   
-         // Remove the anchor element
-         document.body.removeChild(anchor);
-         setInputColor('bg-gray-50 border text-black text-sm rounded-lg block w-80 p-2.5 bg-gray-100 border-gray-600 placeholder-gray-400 text-black mb-4');
-         setErrorTextVisible(false);
-    }catch(error){
+      // Send YouTube URL to the API
+      const response = await axios.post('http://localhost:8080/download', { url }, { responseType: 'blob' });
+
+      console.log(response.data);
+
+      const blob = new Blob([response.data], { type: 'video/mp4' });
+      const blobUrl = URL.createObjectURL(blob);
+      const anchor = document.createElement('a');
+      anchor.href = blobUrl;
+      anchor.download = 'video.mp4';
+      document.body.appendChild(anchor);
+      anchor.click();
+      document.body.removeChild(anchor);
+
+      setInputColor('bg-gray-50 border text-black text-sm rounded-lg block w-80 p-2.5 bg-gray-100 border-gray-600 placeholder-gray-400 text-black mb-4');
+      setErrorTextVisible(false);
+
+    } catch (error) {
       setInputColor('bg-gray-50 border text-black text-sm rounded-lg block w-80 p-2.5 bg-red-50 border-red-500 placeholder-gray-400 text-black mb-4');
       setErrorTextVisible(true);
       console.error('Error downloading video:');
+    } finally {
+      setLoading(false); // Set loading to false after the request is complete (success or failure)
     }
+  };
+
+  const svgStyle = {
+    width: '50px',
+    height: '50px',
   };
 
   return (
     <>
     <div className="flex flex-col items-center justify-center h-screen bg-white text-black">
-      <h1 className="text-3xl font-bold mb-4">YouTube Downloader</h1>
+      <h1 className="text-3xl font-bold ">YouTube Downloader</h1>
       <input
         type="text"
         value={url}
@@ -54,7 +56,15 @@ function App() {
         placeholder="Enter YouTube URL"
         className={inputColor}
       />
-      <a className={isErrorTextVisible ? "justify-center text-red-500 visible " : "justify-center text-red invisible mb-4"}>Invalid URL!</a>
+      {isLoading ? 
+      <div style={svgStyle}>
+        <img src={LoadingSVG} alt="loading" />
+      </div> 
+      : <a className={isErrorTextVisible ? "justify-center text-red-500 visible " : "justify-center text-red invisible mb-4"}>Invalid URL!</a>} 
+
+      {/* Loading indicator */}
+     
+
       <button
         className="bg-red-700 text-white px-6 py-3 rounded font-bold"
         onClick={handleSubmit}
@@ -63,7 +73,7 @@ function App() {
       </button>
     </div>
   </>
-  
+
   )
 }
 
